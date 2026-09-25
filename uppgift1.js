6871b83b96b16485dc4bf6023cfc8396234dc5d2
tillin06 <tios2600@student.miun.se>
2026-09-25 19:08:52 +0200

    skapat variabler, uträkningar och utskrift för uppgift 1 och 2

---
+++ uppgift1.js
@@ -0,0 +1,10 @@
+//Lösning av uppgift 1 av Tilda Öström Linde
+"use strict";
+let firstName = "Tilda";
+let lastName = "Öström Linde";
+let age = "20"
+let student = "true"
+
+console.log (`${firstName} ${lastName}`)
+console.log (`Ålder: ${age}`)
+console.log (`Student: ${student}`)
---
+++ uppgift2.js
@@ -0,0 +1,10 @@
+//Lösning av uppgift 2 beräkna priser av Tilda Öström Linde
+"use strict";
+const price = "100";
+let amount = "3";
+let total = price * amount
+let moms = total * 1.25;
+console.log (`Pris: ${price}`)
+console.log (`Antal: ${amount}`)
+console.log (`Totalt: ${total}`);
+console.log (`Totalt inklusive moms: ${moms}`);
